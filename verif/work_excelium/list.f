56064b9902fdae81b7f4f5de5e0b939f91de2418
ErikaGarciaR <garciarendon.eri@gmail.com>
2026-02-12 13:12:58 -0600

    actualizacion list.f de excelium

--- verif/work_excelium/list.f
+++ verif/work_excelium/list.f
@@ -1,8 +1,15 @@
+../monitoreo_pkg.sv
+../interface_monitoreo.sv
+
 # ============================ rtl
   ../../rtl/monitoreo_top.sv
   ../../rtl/estado_temp.sv
   ../../rtl/comparador_temp.sv
   ../../rtl/registro_temp.sv
+
+# ============================ verificación
+
+../aserciones_monitoreo.sv
   
 # =========================== tb
   ../monitoreo_tb.sv
